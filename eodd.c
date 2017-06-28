f0161dadf4e0b108ed6020e283025ef94f1002e3
ramuklithnes <senthil331999@gmail.com>
2017-06-28 14:45:20 +0530

    Update eodd.c

--- eodd.c
+++ eodd.c
@@ -1,17 +1,21 @@
+
 #include<stdio.h>
 #include<conio.h>
 void main()
 {
-  int a;
-  clrscr();
-  printf("enter the values:");
-  scanf("%d",&a);
-  if(a>=0)
-  {
-    printf("%d" is possitive number);
-  }
-  {
-   printf("%d" is negative number);
-  }
-  getch();
+int a;
+clrscr();
+printf("enter the values:");
+scanf("%d",&a);
+if(a>0)
+{
+printf("the %d is possitive");
+}
+else if(a<0)
+{
+printf("the %d is negative");
+}
+else
+printf("it is zero");
+getch();
 }
