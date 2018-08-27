742adc0fe1bb840155ce25cdbb3d41159fbff6d2
ramuklithnes <senthil331999@gmail.com>
2018-08-27 14:11:57 +0530

    Create Alpha.java

---
+++ Alpha.java
@@ -0,0 +1,38 @@
+import java.io.*;
+
+import java.util.*;
+
+class Alpha
+
+{ 
+ 
+  public static void main(String args[])
+ 
+  {
+  
+   char k;
+  
+   Scanner s=new Scanner(System.in);
+ 
+    k=s.next().charAt(0);
+ 
+    if(k>='a' && k>='A')
+  
+   {
+     
+  System.out.println("Alphabets");
+  
+   }
+     
+else
+ 
+    {
+  
+     System.out.println("Numeric values");
+ 
+    }
+ 
+   }
+  
+  }
+      
