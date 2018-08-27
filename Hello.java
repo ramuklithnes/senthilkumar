0215e05634cc167be3c22fe9da76d193a33053fc
ramuklithnes <senthil331999@gmail.com>
2018-08-27 14:08:19 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+import java.util.*;
+
+class Hello
+
+{
+  
+public static void main(String args[])
+
+  {
+   
+ System.out.println("Enter the number:");
+ 
+   Scanner s=new Scanner(System.in);
+
+    int n=s.nextInt();
+   
+ for(int i=0;i<n;i++)
+  
+  {
+  
+    System.out.println("Hello");
+ 
+   }
+  
+}
+
+ } 
